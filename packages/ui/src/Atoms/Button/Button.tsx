650337b49efdfb16ab5577f66c391440b04cb328
Sheridan SHABANI <sheridan.shabani@outlook.fr>
2022-12-16 16:54:47 +0100

    Atomic Design Base

---
+++ packages/ui/src/Atoms/Button/Button.tsx
@@ -0,0 +1,8 @@
+import Styles from '.\Button.module.scss';
+import {PropsWithChildren} from "react";
+
+export function Button ({ children }: PropsWithChildren<{}>) {
+    return <button className={Styles.Button}>
+        {children}
+    </button>
+};
