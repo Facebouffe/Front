adc43094d9878091209eab06464e71c1a64c9ea3
Ahmed EL HARIRI <ahmed.elhariri@viacesi.fr>
2022-12-28 15:15:39 +0100

    go iÃnput

---
+++ packages/ui/src/Atoms/Forms/Forms.stories.tsx
@@ -0,0 +1,8 @@
+import {Forms} from "./Forms";
+
+export default {
+    title: "Atoms/Forms",
+    component: Forms,
+};
+export const forms = () => <Forms />
+
---
+++ packages/ui/src/Atoms/Forms/Forms.tsx
@@ -0,0 +1,6 @@
+import Styles from "./Forms.module.scss";
+
+
+export function Forms() :any{
+    return    <input className={Styles.Forms}/>
+}
